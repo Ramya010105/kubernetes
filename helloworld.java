af2c284f994cf1a5695548d1a441177031e2ad09
Ramya010105 <bramya1105@gmail.com>
2025-10-22 10:52:02 +0530

    add one file

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
-public class SampleName{
+public class helloworld{
     public static void main(String[] args){
-        String name="Ramya";
+        String name="hello";
         System.out.println("Hello,my name is" +name);
     }
 }
